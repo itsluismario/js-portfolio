ad088bf8d639331ec45d6081fcb5546c1ef8f1ed
Luis Mario <itsluismario@gmail.com>
2023-01-30 12:37:21 -0600

    Loader for images

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
 import getData from '../utils/getData.js';
+import github from '../assets/images/github.png';
+import instagram from '../assets/images/instagram.png';
+import twitter from '../assets/images/twitter.png';
 
 const Template = async () => {
   const data = await getData();
@@ -23,14 +26,14 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="assets/images/twitter.png" />
+          <a href="https://twitter.com/its.luismario">
+            <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
-            <img src="assets/images/github.png" />
+          <a href="https://github.com/its.luismario">
+            <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
-            <img src="assets/images/instagram.png" />
+          <a href="https://instagram.com/its.luismario">
+            <img src="${instagram}" />
           </a>
         </div>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
                     'css-loader',
                     'stylus-loader'
                   ],
+                },
+                {
+                  test: /\.png/,
+                  type: 'asset/resource'
                 }
             ]
   },
